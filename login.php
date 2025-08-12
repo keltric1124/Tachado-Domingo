4850717285148c01d1ee56d9cbeffec63b9b3432
Jefford08 <jeffordjethro@gmail.com>
2025-08-12 23:08:25 +0800

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,16 @@ $password = $_POST['password'];
 $sql = "Select * from Students Where email = '$email' and password = '$password'";
 
 $result =  mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result)>0) {
+   echo "Login Successful!";
+}else{ 
+   echo "invalid email or password.";
+}
+
+mysqli_close($conn);
 ?>
+
 
 
 
